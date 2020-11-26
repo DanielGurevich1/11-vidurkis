b507695b0ab44a61dd75c7e4065c6fca6584a2b5
DanielGurevich1 <73877308+DanielGurevich1@users.noreply.github.com>
2020-11-26 13:18:56 +0200

    done

--- components/validacijos.js
+++ components/validacijos.js
@@ -11,6 +11,10 @@ function arTikrasSarasas(sarasas) {
 }
 
 function arNormalusSkaicius(skaicius, tikrinamoObjektoPavadinimas) {
+    if (typeof tikrinamoObjektoPavadinimas !== 'string') {
+        console.warn('Warning: skaiciaus tipo tikrinimo objekto pavadinimas');
+        tikrinamoObjektoPavadinimas = 'reksme/objektas';
+    }
     if (typeof skaicius !== 'number') {
         console.error(`ERROR: ${tikrinamoObjektoPavadinimas} nera skaicius.`);
             return false;
